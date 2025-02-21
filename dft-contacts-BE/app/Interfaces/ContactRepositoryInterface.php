5cdc44973523da3202c92f06bc68a7787c06cb54
engrmayoronline <engrmayoronline@gmail.com>
2025-02-21 16:49:40 +0000

    Updated the interface classes

--- dft-contacts-BE/app/Interfaces/ContactRepositoryInterface.php
+++ dft-contacts-BE/app/Interfaces/ContactRepositoryInterface.php
@@ -2,7 +2,16 @@
 
 namespace App\Interfaces;
 
+use App\Data\ContactData;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 interface ContactRepositoryInterface
 {
-    //
+    public function getAll(): LengthAwarePaginator;
+    public function create(ContactData $contactData): ContactData;
+    public function update(int $id, ContactData $contactData): ContactData;
+    public function delete(int $id): void;
+    public function findById(int $id): ?ContactData;
+    public function findByEmail(string $email): ContactData;
+    public function search(string $contactData): LengthAwarePaginator;
 }
