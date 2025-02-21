00d424aeee01d1d779096cc8ec7d822bda5339bd
engrmayoronline <engrmayoronline@gmail.com>
2025-02-21 17:06:16 +0000

    added commenting to the interfaces for better understanding

--- dft-contacts-BE/app/Interfaces/ContactRepositoryInterface.php
+++ dft-contacts-BE/app/Interfaces/ContactRepositoryInterface.php
@@ -6,11 +6,52 @@ use App\Data\ContactData;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 interface ContactRepositoryInterface
-{
-    public function getAll(): LengthAwarePaginator;
-    public function create(ContactData $contactData): ContactData;
-    public function update(int $id, ContactData $contactData): ContactData;
-    public function delete(int $id): void;
-    public function findById(int $id): ?ContactData;
+{    
+    /**
+     * Method getAll
+     * Description: Get all contacts
+     * @return LengthAwarePaginator
+     */
+    public function getAll(): LengthAwarePaginator;    
+    /**
+     * Method create
+     * Description: Create a new contact
+     * @param ContactData $contactData [explicite description]
+     *
+     * @return ContactData
+     */
+    public function create(ContactData $contactData): ContactData;    
+    /**
+     * Method update
+     * Description: Update an existing contact
+     * @param int $id [explicite description]
+     * @param ContactData $contactData [explicite description]
+     *
+     * @return ContactData
+     */
+    public function update(int $id, ContactData $contactData): ContactData;    
+    /**
+     * Method delete
+     * Description: Delete an existing contact
+     * @param int $id [explicite description]
+     *
+     * @return void
+     */
+    public function delete(int $id): void;    
+    /**
+     * Method findById
+     * Description: Find a contact by its ID
+     * @param int $id [explicite description]
+     *
+     * @return ContactData
+     */
+    public function findById(int $id): ?ContactData;    
+    /**
+     * Method search
+     * Description: Search for contacts by a given search term
+     * @param string $contactData [explicite description]
+     *
+     * @return LengthAwarePaginator
+     */
     public function search(string $contactData): LengthAwarePaginator;
 }
--- dft-contacts-BE/app/Interfaces/ContactServiceInterface.php
+++ dft-contacts-BE/app/Interfaces/ContactServiceInterface.php
@@ -6,11 +6,57 @@ use App\Data\ContactData;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 interface ContactServiceInterface
-{
+{    
+    /**
+     * Method getAllContacts
+     * Description: Get all contacts
+     * @return LengthAwarePaginator
+     */
     public function getAllContacts(): LengthAwarePaginator;
+        
+    /**
+     * Method createContact
+     * Description: Create a new contact
+     * @param ContactData $contactData [explicite description]
+     *
+     * @return ContactData
+     */
     public function createContact(ContactData $contactData): ContactData;
+        
+    /**
+     * Method updateContact
+     * Description: Update an existing contact
+     * @param int $id [explicite description]
+     * @param ContactData $contactData [explicite description]
+     *
+     * @return ContactData
+     */
     public function updateContact(int $id, ContactData $contactData): ContactData;
+        
+    /**
+     * Method deleteContact
+     * Description: Delete an existing contact
+     * @param int $id [explicite description]
+     *
+     * @return void
+     */
     public function deleteContact(int $id): void;
+        
+    /**
+     * Method findContactById
+     * Description: Find a contact by its ID
+     * @param int $id [explicite description]
+     *
+     * @return ContactData
+     */
     public function findContactById(int $id): ?ContactData;
+        
+    /**
+     * Method searchContacts
+     * Description: Search for contacts by a given search term
+     * @param string $contactData [explicite description]
+     *
+     * @return LengthAwarePaginator
+     */
     public function searchContacts(string $contactData): LengthAwarePaginator;
 }
