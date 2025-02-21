f1cd0d737addf7dadb12fabe427281d6835efa2c
engrmayoronline <engrmayoronline@gmail.com>
2025-02-21 15:10:07 +0000

    implemented the spatie data with a contactdata class and also added the fillables to the model

---
+++ dft-contacts-BE/app/Data/ContactData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\LaravelData\Data;
+
+class ContactData extends Data
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(
+        public string $fname,
+        public string $lname,
+        public string $email,
+        public string $phone,
+        public ?string $address
+    ){}
+
+    public static function rules(): array
+    {
+        return [
+            'fname' => ['required', 'string'],
+            'lname' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'phone' => ['required', 'string', 'max:20'],
+            'address' => ['nullable', 'string']
+        ];
+    }
+}
