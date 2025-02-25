cf750310e123dd1305d104bf9387ab23a935b04f
engrmayoronline <engrmayoronline@gmail.com>
2025-02-25 22:28:25 +0000

    Update the route file

---
+++ dft-contacts-BE/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/contacts', [ContactController::class, 'index']);
+Route::post('/contacts', [ContactController::class, 'store']);
+Route::get('/contacts/{id}', [ContactController::class, 'show']);
+Route::put('/contacts/{id}', [ContactController::class, 'update']);
+Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
+Route::get('/contacts/search/{term}', [ContactController::class, 'search']);
